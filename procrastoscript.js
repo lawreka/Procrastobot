1df2f8bd78d1fab880e4853f28c5b67147e68040
Kathryn Isabelle Lawrence <kathrynlawrence@bln-imac-475.hmkw-ad.de>
2016-11-03 15:52:29 +0100

    no reload, pupils for main faces

--- procrastoscript.js
+++ procrastoscript.js
@@ -19,22 +19,6 @@ function minSec(i) {
     return i;
   }
 }
-// assign task input to popup window text
-// var get = document.querySelector("#settext");
-// get.addEventListener("submit", setText);
-// function setText(event){
-//   event.preventDefault();
-//   var text = document.querySelector("#txt");
-//   var t = text.value;
-//   if (t == ""){
-//     window.alert("You must set a task");
-//   }
-//   else{
-//     console.log(t);
-//     $("#settext").addClass("hidden");
-//     $("#todo").text(t);
-//   }
-// }
 //dropdown functions
 function pick(){
   $("#times").removeClass("hidden");
@@ -44,12 +28,7 @@ window.onclick = function(event){
     $("#times").addClass("hidden");
   }
 }
-// assign reminder setter time to clock time and schedule popups
-// function checkTime(){
-//   var currentTime = new Date();
-//   console.log(currentTime);
-//   var t = setTimeout(checkTime, 2000);
-// }
+// schedule popups
 function fivemins(){
   if ($("#posicon").hasClass("notselect") && $("#neuticon").hasClass("notselect") && $("#negaicon").hasClass("notselect")){
     window.alert("Please choose a mood for your reminder");
@@ -79,16 +58,37 @@ function fivemins(){
     $("#moodselect").addClass("superhide");
     $("#faces").removeClass("hidden");
     window.alert("Remind me in 5 minutes");
-    var t = setTimeout(fivemins, 2000);
+    var t = setTimeout(fivemins, 30000);
   }
 }
 function thirtmins(){
   if ($("#posicon").hasClass("notselect") && $("#neuticon").hasClass("notselect") && $("#negaicon").hasClass("notselect")){
     window.alert("Please choose a mood for your reminder");
+    return;
   }
   else{
+    if (mood == posi){
+          $("#posiface").removeClass("superhide");
+          $("#posiface").addClass("mainface");
+    }
+    else{
+      if (mood == neut){
+        $("#neutface").removeClass("superhide");
+        $("#neutface").addClass("mainface");
+      }
+      else{
+        if (mood == nega){
+          $("#negaface").removeClass("superhide");
+          $("#negaface").addClass("mainface");
+        }
+      }
+    }
+    $("#clock").removeClass("clock");
+    $("#clock").addClass("smallclock");
     $("#dropdown").addClass("hidden");
-    var msg = document.querySelector("#todo").textContent;
+    $("#todo").addClass("superhide");
+    $("#moodselect").addClass("superhide");
+    $("#faces").removeClass("hidden");
     window.alert("Remind me in 30 minutes");
     var t = setTimeout(thirtmins,  1800000 );
   }
@@ -96,15 +96,36 @@ function thirtmins(){
 function onehour(){
   if ($("#posicon").hasClass("notselect") && $("#neuticon").hasClass("notselect") && $("#negaicon").hasClass("notselect")){
     window.alert("Please choose a mood for your reminder");
+    return;
   }
   else{
+    if (mood == posi){
+          $("#posiface").removeClass("superhide");
+          $("#posiface").addClass("mainface");
+    }
+    else{
+      if (mood == neut){
+        $("#neutface").removeClass("superhide");
+        $("#neutface").addClass("mainface");
+      }
+      else{
+        if (mood == nega){
+          $("#negaface").removeClass("superhide");
+          $("#negaface").addClass("mainface");
+        }
+      }
+    }
+    $("#clock").removeClass("clock");
+    $("#clock").addClass("smallclock");
     $("#dropdown").addClass("hidden");
-    var msg = document.querySelector("#todo").textContent;
+    $("#todo").addClass("superhide");
+    $("#moodselect").addClass("superhide");
+    $("#faces").removeClass("hidden");
     window.alert("Remind me in 1 hour");
     var t = setTimeout(fivemins, 3600000);
   }
 }
-// use mood changer to change popup window text
+// change mood
 var mood = "";
 function posi(){
   $("#posicon").removeClass("notselect");
@@ -127,7 +148,3 @@ function nega(){
   $("#neut").addClass("hidden");
   mood = nega;
 }
-// reset button to clear all inputs
-$("#reset").on("click", function(){
-  window.location.reload(true);
-})
